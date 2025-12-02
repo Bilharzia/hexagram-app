86dc3be824e6796a3cfbcb6a13dd2740f9e08a53
Eryck Milano <eryckmilano@gmail.com>
2025-12-02 16:16:44 -0300

    fix: honeycomb visible with proper Tailwind safelisting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+    "./app/components/**/*.{rb,erb}"  // ← importante
   ],
-  theme: { extend: {} },
-  plugins: []
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
